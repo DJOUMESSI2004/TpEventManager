34c69fc7e57b39056fb0f83748b1c231c2725695
wilfrid <wilfrid.ndongmodjoumessi@fanuc.eu>
2025-04-30 14:49:01 +0200

    added model

--- eventmanager/events/models.py
+++ eventmanager/events/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def confirmed_participants_count(self):
+        return self.participations.filter(will_come=True).count()
+
+class Participation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, related_name="participations", on_delete=models.CASCADE)
+    will_come = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('user', 'event')
